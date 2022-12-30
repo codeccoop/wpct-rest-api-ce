49bd4f4ebac29c3206ba013049c2ecf9e4d562ae
Lucas García <lucas@codeccoop.org>
2022-12-30 11:40:15 +0100

    feat: clean up single template

--- templates/single.php
+++ templates/single.php
@@ -14,16 +14,5 @@ wp_head();
     <main class="site-ce-content site-content" style="margin-top: 0; padding-top: 0;"><?php the_content(); ?></main>
     <footer class="site-footer"><?php block_template_part('footer'); ?></footer>
 </div>
-
-<!-- wp:group {"tagName":"main","style":{"spacing":{"margin":{"top":"0"},"padding":{"top":"0"}}},"className":"site-content"} -->
-<main class="site-ce-content wp-block-group" style="margin-top:0;padding-top:var(--wp--custom--spacing--sxl);padding-bottom:var(--wp--custom--spacing--sxxl)">
-    <!-- wp:post-content {"layout":{"inherit":true}} /-->
-</main>
-<!-- /wp:group -->
-<!-- wp:template-part {"slug":"footer","tagName":"footer","className":"site-footer"} /-->
-
-<h1>I'm a comunity single template</h1>
-<?php $odoo_community_id = get_post_meta(get_the_ID(), 'odoo_community_id', true); ?>
-<strong><?= $odoo_community_id ?></strong>
 <?php wp_footer(); ?>
 </body>
